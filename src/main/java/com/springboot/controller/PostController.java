0cdfbeb57aa5e1e05c804f9675bc9262c945cf63
sanghwa <show1003272@gmail.com>
2022-08-14 17:56:48 +0900

    데이터 검증1 - 완료

--- src/main/java/com/springboot/controller/PostController.java
+++ src/main/java/com/springboot/controller/PostController.java
@@ -2,11 +2,16 @@ package com.springboot.controller;
 
 import com.springboot.request.PostCreate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -16,9 +21,20 @@ public class PostController {
     // 글 등록으로는 POST Method 사용
 
     @GetMapping("/posts")
-    public String post(@RequestBody PostCreate params){
-        log.info("params={}", params.toString());
-        return "Hellow Spring Boot";
+    // @Valid - 설정된 값을 검증해준다.
+    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result){
+        if(result.hasErrors()){
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            FieldError firstFieldError = fieldErrors.get(0);
+            String fieldName = firstFieldError.getField(); // title
+            String errorMessage = firstFieldError.getDefaultMessage(); // error message
+            Map<String, String> error = new HashMap<>();
+            error.put(fieldName, errorMessage);
+            return error;
+        }
+        return Map.of();
+//        log.info("params={}", params.toString());
+//        return "Hellow Spring Boot";
     }
 
 
--- src/main/java/com/springboot/request/PostCreate.java
+++ src/main/java/com/springboot/request/PostCreate.java
@@ -1,7 +1,11 @@
 // request === DTO 라고 생각하면 된다.
 package com.springboot.request;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
 
 
 //@ToString
@@ -10,26 +14,31 @@ import lombok.ToString;
 // ToString, Get, Set 자동완성 단축키
 //      Windows -> alt + insert
 //      Mac -> 후에 작성하겠음
-// 혹은 만들어줄 필요없이 ToString, Set, Get 어노테이션을 선언하면 자동으로 주입하여 사용가능하다.
+// 혹은 만들어줄 필요없이 ToString, Setter, Getter 어노테이션을 선언하면 자동으로 주입하여 사용가능하다.
+@ToString
+@Setter
+@Getter
 public class PostCreate {
+    @NotBlank(message = "타이틀이 누락되었습니다")           // 자동으로 빈 값을 검증해줌(null 포함)
     public String title;
+    @NotBlank(message = "내용이 누락되었습니도")
     public String content;
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
 
     // PostController에서 toString 메서드를 사용하기 때문에 만들어줘야 한다
     // 후에 테스트 코드를 실행해보면 해당 params가 null로 나오는데 set 메서드를 만들어주면 된다.
-    @Override
-    public String toString() {
-        return "PostCreate{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "PostCreate{" +
+//                "title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 }
